732fcc9de42e252633f853cf2c3313257ce824c9
Amit Sharma <amitksharma@gmail.com>
2020-06-18 17:33:27 +0530

    shortcode atts

--- playing-card-notations.php
+++ playing-card-notations.php
@@ -11,11 +11,16 @@
 add_shortcode('pcards', 'add_playing_card_notations');
 
 function add_playing_card_notations($atts) {
-	// $atts
+	$a = shortcode_atts( array(
+			'content'		=> 'AhKc',
+			'layout'		=> 'inline',
+		), $atts);
+
+
 	/* atts
 		c = content, e.g. Ac7d
 		layout = in, left, right, centre
-		
+	*/
 	/*
 		A K Q J T/10 9 8 7 6 5 4 3 2
 		s c h d
@@ -25,16 +30,21 @@ function add_playing_card_notations($atts) {
 		&diams;
 		&#x1f0a1;
 	*/
-	$suites = [
-		"s" => "&spades;",
-		"c" => "&clubs;",
-		"h" => "&hearts;",
-		"d" => "&diams;"
-	];
-	$out = $atts; // have to do this properly
-	foreach ($suites as $key => $value) {
-		// code...
-		$out = str_replace(&key, &value, $out);
+	if ($a->layout == "inline") {
+		// unicode cards
+	} else {
+		// rich cards, non-inline	
+		$suites = [
+			"s" => "<span class='pc-suite pc-suite-spades pc-suite-blacks'>&spades;</span>",
+			"c" => "<span class='pc-suite pc-suite-clubs pc-suite-blacks'>&clubs;</span>",
+			"h" => "<span class='pc-suite pc-suite-hearts pc-suite-reds'>&hearts;</span>",
+			"d" => "<span class='pc-suite pc-suite-diams pc-suite-reds'>&diams;</span>",
+		];
+		$out = $a->content; 
+		foreach ($suites as $key => $value) {
+			// replace the suite keys with suite code
+			$out = str_replace(&key, &value, $out);
+		}
 	}
 	$out = "<span class='pcards-block'>" . $out . "</span>";
 	return $out;
